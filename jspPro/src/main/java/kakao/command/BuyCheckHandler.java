025e52ffe8c4b98c82b097b64aa982da3238470d
이상호/LeeSangho <yredy@naver.com>
2022-07-07 11:50:30 +0900

    Add files via upload

---
+++ jspPro/src/main/java/kakao/command/BuyCheckHandler.java
@@ -0,0 +1,31 @@
+package command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.BuyCheckService;
+
+
+public class BuyCheckHandler implements CommandHandler {
+
+	@Override
+	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		int bl_num = Integer.parseInt(request.getParameter("bl_num"));
+		String bl_id = request.getParameter("bl_id");
+		
+		System.out.println("buyCheck 핸들러 호출");
+		
+		BuyCheckService buyCheckService = BuyCheckService.getInstance();
+		int rowCount = buyCheckService.buyCheck(bl_num, bl_id);
+		System.out.println("buyCheck 메소드 호출");
+		
+		/*
+		 * String location = "/jspPro/kview/view_buyCheck.jsp?rowCount="+rowCount;
+		 * response.sendRedirect(location); // 포워딩 X, 리다이렉트 O
+		 * System.out.println(rowCount);
+		 */		
+		
+		return "/kview/view_buyCheck.jsp?rowCount="+rowCount;
+	}
+
+}
