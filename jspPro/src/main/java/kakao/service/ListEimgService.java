b12a1578ae524d1c9cd7110c6d71c7f6b487e21c
doseonghan <106295756+doseonghan@users.noreply.github.com>
2022-07-09 14:17:24 +0900

    Add files via upload

---
+++ jspPro/src/main/java/kakao/service/ListEimgService.java
@@ -0,0 +1,43 @@
+package kakao.service;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.naming.NamingException;
+
+import com.util.ConnectionProvider;
+import com.util.JdbcUtil;
+
+import kakao.domain.EimgDTO;
+import kakao.persistence.EimgDAO;
+
+public class ListEimgService {
+
+	// 1. 싱글톤
+	private ListEimgService() {}   // ㄱ. private 생성자 선언  이유? 외부 객체 생성 못하게하기 위헤
+	private static ListEimgService instance = null; // ㄴ. private static 객체 변수
+	public static ListEimgService getInstance() {  // ㄷ. public static 메서드
+		if( instance == null ) {
+			instance = new ListEimgService();
+		}
+		return instance;
+	}
+
+	public List<EimgDTO> selectEimgList(){
+		//
+		Connection con = null;
+		try {
+			con = ConnectionProvider.getConnection();
+			EimgDAO dao = EimgDAO.getInstance();
+			List<EimgDTO> list = null;
+			list = dao.selectEimgList(con);
+			return list;
+		} catch (NamingException | SQLException e) { 
+			throw new RuntimeException(e);
+		} finally {
+			JdbcUtil.close(con);
+		}
+	}
+
+}
